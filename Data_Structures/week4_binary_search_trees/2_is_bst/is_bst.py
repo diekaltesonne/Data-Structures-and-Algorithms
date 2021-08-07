b6ef1d3607cb7e75140c5da763618ca237dc8480
Ilya Trifonov <ilya.trifonov@zyfra.com>
2021-08-07 18:24:18 +0300

    udpate 07/08/21

--- Data_Structures/week4_binary_search_trees/2_is_bst/is_bst.py
+++ Data_Structures/week4_binary_search_trees/2_is_bst/is_bst.py
@@ -1,20 +1,31 @@
-#!/usr/bin/python3
+
 import sys, threading
-sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+sys.setrecursionlimit(10**8) # max depth of recursion
+threading.stack_size(2**27)  # new thread will get stack of such size
+
+class TreeChecker():
+  
+  def __init__(self,tree) -> None:
+       self.tree = tree
+  
+  def isBSTUtil(self, i, mini, maxi):     
+      if i == -1:
+          return True
+      # False if this node violates min/max constraint
+      if self.tree[i][0] < mini or self.tree[i][0] > maxi:
+          return False
+      # Otherwise check the subtrees recursively
+      # tightening the min or max constraint
+      return (self.isBSTUtil(self.tree[i][1],mini, self.tree[i][0]-1) and
+            self.isBSTUtil(self.tree[i][2], self.tree[i][0]+1, maxi))
+
+  def isBST(self):
+    if len(self.tree) == 0:
+      return True
+    return (self.isBSTUtil(0, -4294967296, 4294967296))
 
 def IsBinarySearchTree(tree):
-  ans = False
-  if len(tree) == 0:
-    return True
-  for i in tree:
-    if ((i[1] < i[0]) or (i[1] == -1)) and ((i[0] <= i[2]) or (i[2] == -1)):
-      ans = True
-    else:
-      ans = False
-      break
-  return ans
-
+   return TreeChecker(tree).isBST()
 
 def main():
   nodes = int(sys.stdin.readline().strip())
